a12128e8a3b4b5ad1caababb84e1dd281869fa91
thangtm2 <thangtm2@vng.com.vn>
2024-03-05 13:55:20 +0700

    Update display

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="container">
       <div className="top">
-        <h1>Scroll to End</h1>
+        <h1 className="text-align-center san-serif">Scroll to End</h1>
+        <div className="description text-align-center san-serif">The file chunk of the component Random User is only loaded when scrolling to the end of the page.</div>
+        <p className="text-align-center san-serif"><a href="https://github.com/thangtmc73/react-intersection-observer-example">Source Code</a></p>
       </div>
       <div className="down-arrow"></div>
       <LoadInViewportWrapper>
